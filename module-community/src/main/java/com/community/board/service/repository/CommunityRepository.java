f44d89ecea3eb88db3df12ed4b7c17ab0699cc73
K-KY <kyuyoungk@naver.com>
2025-12-19 06:45:26 +0900

    feat: tab 아이디로 게시글 리스트 조회

--- module-community/src/main/java/com/community/board/service/repository/CommunityRepository.java
+++ module-community/src/main/java/com/community/board/service/repository/CommunityRepository.java
@@ -1,7 +1,11 @@
 package com.community.board.service.repository;
 
 import com.community.board.service.entity.Community;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface CommunityRepository extends JpaRepository<Community, Long> {
+    Page<Community> findAllByCommunityTab_CommunityTabSeq(Long communityTabCommunityTabSeq, Pageable pageable);
 }
