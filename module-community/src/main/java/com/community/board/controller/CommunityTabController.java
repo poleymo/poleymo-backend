067d7cffc42c62dce75ea23891fa1d754e4bab8b
K-KY <kyuyoungk@naver.com>
2025-12-20 03:02:22 +0900

    feat: 커뮤니티 탭 수정 삭제 기능 추가

--- module-community/src/main/java/com/community/board/controller/CommunityTabController.java
+++ module-community/src/main/java/com/community/board/controller/CommunityTabController.java
@@ -4,6 +4,8 @@ import com.community.board.service.CommunityTabService;
 import com.community.board.service.dto.CommunityTabDto;
 import com.community.board.service.entity.CommunityTab;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,8 +25,20 @@ public class CommunityTabController {
     }
 
     @PostMapping
-    public CommunityTabDto.Response saveTab(@RequestBody CommunityTabDto.Request tab) {
-        CommunityTab communityTab = communityTabService.saveTab(tab);
+    public CommunityTabDto.Response saveTab(@RequestBody CommunityTabDto.Request dto) {
+        CommunityTab communityTab = communityTabService.saveTab(dto);
         return CommunityTabDto.from(communityTab);
+    }
+
+    @PatchMapping
+    public CommunityTabDto.Response updateTab(@RequestBody CommunityTabDto.Update dto) {
+        CommunityTab communityTab = communityTabService.updateTab(dto);
+        return CommunityTabDto.from(communityTab);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<HttpStatus> deleteTab(@RequestBody CommunityTabDto.Delete dto) {
+        communityTabService.deleteTab(dto);
+        return ResponseEntity.ok().body(HttpStatus.OK);
     }
 }
