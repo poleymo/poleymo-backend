81f36ac76838027532f27a557256acf35e9f6657
K-KY <kyuyoungk@naver.com>
2025-12-20 02:53:39 +0900

    feat: 엔티티 내에 엔티티 수정을 위한 메서드 추가

--- module-community/src/main/java/com/community/board/service/entity/CommunityTab.java
+++ module-community/src/main/java/com/community/board/service/entity/CommunityTab.java
@@ -7,6 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
+import java.util.Objects;
+
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
@@ -21,4 +23,18 @@ public class CommunityTab extends BaseTimeEntity {
     private String tabName;
 
     private Boolean visible;
+
+    public void changeVisible(Boolean visible) {
+        if (Objects.equals(this.visible, visible)) {
+            return;
+        }
+        this.visible = visible;
+    }
+
+    public void changeTabName(String tabName) {
+        if (Objects.equals(this.tabName, tabName)) {
+            return;
+        }
+        this.tabName = tabName;
+    }
 }
