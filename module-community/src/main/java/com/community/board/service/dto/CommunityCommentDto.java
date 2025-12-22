237de8ce88a5bae6a540a5551360fd3e70d96007
K-KY <kyuyoungk@naver.com>
2025-12-22 21:32:08 +0900

    feat: 게시글 댓글 기능 dto
    - 조회수 필드 추가 해야함
    - recommend 기능과 사용자 기능 연결 해야함

---
+++ module-community/src/main/java/com/community/board/service/dto/CommunityCommentDto.java
@@ -0,0 +1,57 @@
+package com.community.board.service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class CommunityCommentDto {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class Request {
+        private String author;// 사용자
+        private Long communitySeq;// 게시글 아이디
+        private Long parentId; // 부모 아이디
+        private String content; // 내용
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class Response {
+        private String author;
+        private Long communitySeq;
+        private Long parentSeq;
+        private Long commentSeq;
+        private String content;
+        private Boolean visible;
+        private Long recommend;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class Update {
+        private String author;//변경 불가
+        private Long communitySeq;//변경 불가
+        private Long parentSeq;//변경 불가
+        private Long commentSeq;//변경 불가
+        private String content;
+        private Boolean visible;
+        private Long recommend;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class Delete {
+        private String author;
+        private Long communitySeq;
+        private Long parentSeq;
+        private Long commentSeq;
+        private String content;
+        private Boolean visible;
+        private Long recommend;
+    }
+}
