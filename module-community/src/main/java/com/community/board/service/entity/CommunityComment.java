b7111ad51a6747e45ad53872a892af8031ca384e
K-KY <kyuyoungk@naver.com>
2025-12-22 21:33:10 +0900

    feat: 댓글 기능 엔티티

---
+++ module-community/src/main/java/com/community/board/service/entity/CommunityComment.java
@@ -0,0 +1,43 @@
+package com.community.board.service.entity;
+
+import com.community.util.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class CommunityComment extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentSeq;
+
+    private String author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private CommunityComment parent;
+
+    @ManyToOne
+    private Community community;
+
+    private String content;
+
+    private Boolean visible;
+
+    private Long recommend;
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
+
+    public void changeVisible(Boolean visible) {
+        this.visible = visible;
+    }
+
+    public void changeRecommend(Long recommend) {
+        this.recommend = recommend;
+    }
+}
+
