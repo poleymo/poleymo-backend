f80c5fe4926cf58da05fd6089d95a1a4a6c2c318
K-KY <kyuyoungk@naver.com>
2025-12-22 21:55:09 +0900

    feat: 커뮤니티 댓글 컨트롤러

---
+++ module-community/src/main/java/com/community/board/controller/CommunityCommentController.java
@@ -0,0 +1,41 @@
+package com.community.board.controller;
+
+import com.community.board.service.CommunityCommentService;
+import com.community.board.service.dto.CommunityCommentDto;
+import com.community.board.service.dto.PageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("community/reply")
+@RestController
+public class CommunityCommentController {
+
+    private final CommunityCommentService commentService;
+
+    @GetMapping
+    public PageResponse<CommunityCommentDto.Response> findReplies(Long communitySeq, int page, int size) {
+        Page<CommunityCommentDto.Response> map = commentService.findCommentList(communitySeq, page, size)
+                .map(CommunityCommentDto::from);
+        return PageResponse.from(map);
+    }
+
+    @PostMapping
+    public CommunityCommentDto.Response saveReply(Long communitySeq, CommunityCommentDto.Request dto) {
+        return CommunityCommentDto.from(commentService.saveComment(communitySeq, dto));
+    }
+
+    @PatchMapping
+    public CommunityCommentDto.Response updateReply(Long communitySeq, CommunityCommentDto.Update dto) {
+        return CommunityCommentDto.from(commentService.updateComment(communitySeq, dto));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<HttpStatus> deleteReply(CommunityCommentDto.Delete dto) {
+        commentService.deleteComment(dto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
