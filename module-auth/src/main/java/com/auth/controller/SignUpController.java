101280700cd97719965fdd5bd25be1c957516e9b
K-KY <kyuyoungk@naver.com>
2025-12-26 22:50:52 +0900

    feat: 사용자 인증 정보 임시저장 컨트롤러 스켈레톤

---
+++ module-auth/src/main/java/com/auth/controller/SignUpController.java
@@ -0,0 +1,29 @@
+package com.auth.controller;
+
+import com.auth.dto.UserAuthDto;
+import com.auth.service.UserAuthRedisService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("signup")
+@RequiredArgsConstructor
+public class SignUpController {
+    private final UserAuthRedisService userAuthRedisService;
+
+    @PostMapping
+    public String signUp(UserAuthDto.Request dto) throws JsonProcessingException {
+        return userAuthRedisService.saveTempAuth(dto);
+    }
+
+    @GetMapping
+    public String verify(String token) throws JsonProcessingException {
+        userAuthRedisService.getTempAuth(token);
+        //여기서 영속화
+        return "ok";
+    }
+}
