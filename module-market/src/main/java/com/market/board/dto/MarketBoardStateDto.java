6553cb94da2f0ce4699f1706cfce2d6952745640
hjn5018 <hjn5018@gmail.com>
2025-12-26 19:24:38 +0900

    chore: 중고나라 게시글 상태 주석 예시 추가

--- module-market/src/main/java/com/market/board/dto/MarketBoardStateDto.java
+++ module-market/src/main/java/com/market/board/dto/MarketBoardStateDto.java
@@ -10,7 +10,7 @@ public class MarketBoardStateDto {
     @Builder
     @AllArgsConstructor
     public static class Request {
-        private String mbState; // 중고나라 게시글 상태 (market_board_state)
+        private String mbState; // 중고나라 게시글 상태 (market_board_state) (거래 대기, 거래 중, 거래 종료)
     }
 
     @Getter
@@ -18,7 +18,7 @@ public class MarketBoardStateDto {
     @AllArgsConstructor
     public static class Response {
         private int mbsSeq; // 중고나라 게시글 상태 키 (market_board_state_sequence)
-        private String mbState; // 중고나라 게시글 상태 (market_board_state)
+        private String mbState; // 중고나라 게시글 상태 (market_board_state) (거래 대기, 거래 중, 거래 종료)
         private Boolean visible; // 중고나라 게시글 상태 조회 가능 여부
     }
 }
--- module-market/src/main/java/com/market/board/entity/MarketBoardState.java
+++ module-market/src/main/java/com/market/board/entity/MarketBoardState.java
@@ -17,6 +17,6 @@ public class MarketBoardState {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int mbsSeq; // 중고나라 게시글 상태 키 (market_board_state_sequence)
 
-    private String mbState; // 중고나라 게시글 상태 (market_board_state)
+    private String mbState; // 중고나라 게시글 상태 (market_board_state) (거래 대기, 거래 중, 거래 종료)
     private Boolean visible; // 중고나라 게시글 상태 조회 가능 여부
 }
