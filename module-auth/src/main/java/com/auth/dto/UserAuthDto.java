8652712f12759819fc3faf18dfc39b4b7eb1877b
K-KY <kyuyoungk@naver.com>
2026-01-02 20:45:17 +0900

    feat: 사용자 인증 데이터 응답 dto 추가

--- module-auth/src/main/java/com/auth/dto/UserAuthDto.java
+++ module-auth/src/main/java/com/auth/dto/UserAuthDto.java
@@ -1,5 +1,6 @@
 package com.auth.dto;
 
+import com.auth.entity.UserAuth;
 import lombok.*;
 
 public class UserAuthDto {
@@ -12,5 +13,17 @@ public class UserAuthDto {
         private String userEmail;
         private String password;
         private Boolean active;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Response {
+        private String userEmail;
+        private Boolean active;
+    }
+
+    public static Response of(UserAuth userAuth) {
+        return new Response(userAuth.getUserEmail(), userAuth.getActive());
     }
 }
