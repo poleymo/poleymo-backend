4266f7d1876ab1cc7880cfbb0b0d665c35a155fc
K-KY <kyuyoungk@naver.com>
2026-01-02 21:35:14 +0900

    feat: 중복 여부 조회, active상태인 email과 password가 일치하는 데이터 조회

--- module-auth/src/main/java/com/auth/repository/UserAuthRepository.java
+++ module-auth/src/main/java/com/auth/repository/UserAuthRepository.java
@@ -3,5 +3,10 @@ package com.auth.repository;
 import com.auth.entity.UserAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserAuthRepository extends JpaRepository<UserAuth, Long> {
+    boolean existsByUserEmail(String userEmail);
+
+    Optional<UserAuth> findByUserEmailAndPasswordAndActive(String userEmail, String password, Boolean active);
 }
