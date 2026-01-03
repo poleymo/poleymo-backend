061ac57c14072c07e829988b32e9f3ab2c45b4f9
K-KY <kyuyoungk@naver.com>
2026-01-03 19:40:12 +0900

    feat: 회원 가입시 메일을 같이 보내도록 수정

--- module-auth/src/main/java/com/auth/controller/SignUpController.java
+++ module-auth/src/main/java/com/auth/controller/SignUpController.java
@@ -2,15 +2,14 @@ package com.auth.controller;
 
 import com.auth.dto.UserAuthDto;
 import com.auth.entity.UserAuth;
+import com.auth.service.MailService;
 import com.auth.service.PasswordEncryptor;
 import com.auth.service.UserAuthRedisService;
 import com.auth.service.impl.UserAuthService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("signup")
@@ -19,11 +18,13 @@ public class SignUpController {
     private final UserAuthRedisService userAuthRedisService;
     private final UserAuthService userAuthService;
     private final PasswordEncryptor passwordEncryptor;
+    private final MailService mailService;
 
     @PostMapping
-    public String signUp(UserAuthDto.Request dto) throws JsonProcessingException {
-        //todo: 이메일 전송
-        return userAuthRedisService.saveTempAuth(passwordEncryptor.encrypt(dto));
+    public String signUp(@RequestBody UserAuthDto.Request dto) throws JsonProcessingException, MessagingException {
+        String token = userAuthRedisService.saveTempAuth(passwordEncryptor.encrypt(dto));
+        mailService.sendMail(dto.getUserEmail(), "폴리모 회원가입 인증 메일", token);
+        return token;
     }
 
     @GetMapping
