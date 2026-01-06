7dace378e3da2a18eafd3ac76b0d280ff0e08ff3
K-KY <kyuyoungk@naver.com>
2026-01-06 23:38:14 +0900

    feat: UserAuthService에 UserDetailsService 상속

--- module-auth/src/main/java/com/auth/service/impl/UserAuthService.java
+++ module-auth/src/main/java/com/auth/service/impl/UserAuthService.java
@@ -2,8 +2,9 @@ package com.auth.service.impl;
 
 import com.auth.dto.UserAuthDto;
 import com.auth.entity.UserAuth;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface UserAuthService {
+public interface UserAuthService extends UserDetailsService {
     UserAuth saveAuth(UserAuthDto.Request dto);
     UserAuth findAuth(UserAuthDto.Request dto);
     UserAuth updateAuth(UserAuthDto.Update dto);
