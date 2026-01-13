efe5919a6f3b1e8d60b497e5aedc32503111a298
K-KY <kyuyoungk@naver.com>
2026-01-13 21:22:23 +0900

    feat: 아이디로 사용자 정보 조회 메서드 추가

--- module-auth/src/main/java/com/auth/service/impl/UserAuthService.java
+++ module-auth/src/main/java/com/auth/service/impl/UserAuthService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 public interface UserAuthService extends UserDetailsService {
     UserAuth saveAuth(UserAuthDto.Request dto);
     UserAuth findAuth(UserAuthDto.Request dto);
+    UserAuth findAuthById(Long id);
     UserAuth updateAuth(UserAuthDto.Update dto);
     void deleteAuth(UserAuthDto.Delete dto);
 }
