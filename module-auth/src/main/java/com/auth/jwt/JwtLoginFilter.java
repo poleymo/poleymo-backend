27b50e2dcb4506f487d4d8f424bfa1eabbb55899
K-KY <kyuyoungk@naver.com>
2026-01-16 23:30:19 +0900

    refactor: 로그인 시 리프레시 토큰을 쿠키로 넣어주도록 변경
    - 기존 바디로 넣어 보냈으나 이 방법은 리프레시 토큰을 document.cookie 등으로 꺼낼 수 있음
    - 이 문제 방지하기 위해 쿠키로 넣는 방식으로 변경

--- module-auth/src/main/java/com/auth/jwt/JwtLoginFilter.java
+++ module-auth/src/main/java/com/auth/jwt/JwtLoginFilter.java
@@ -8,6 +8,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,6 +18,7 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Map;
 
 public class JwtLoginFilter extends UsernamePasswordAuthenticationFilter {
@@ -43,6 +46,7 @@ public class JwtLoginFilter extends UsernamePasswordAuthenticationFilter {
         }
     }
 
+    //todo : role 관리
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
                                             FilterChain chain, Authentication authResult)
@@ -51,14 +55,27 @@ public class JwtLoginFilter extends UsernamePasswordAuthenticationFilter {
 
         String at = jwtService.createAccessToken(principal.getAuthSeq(), principal.getUsername(), "ROLE_USER");
         String rt = jwtService.createRefreshToken(principal.getAuthSeq());
+        ResponseCookie refreshTokenCookie = createRefreshTokenCookie(rt);
 
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
 
+        response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+
         response.getWriter().write(
                 new ObjectMapper().writeValueAsString(
-                        Map.of("accessToken", at, "refreshToken", rt)
+                        Map.of("accessToken", at)
                 )
         );
+    }
+
+    private ResponseCookie createRefreshTokenCookie(String refreshToken) {
+        return ResponseCookie.from("refresh_token", refreshToken)
+                .maxAge(Duration.ofDays(30))
+                .httpOnly(true)//개발자 콘솔에서 읽지 못하게
+                .secure(false)//https 필수 옵션인데 지금 없으니 일단 false
+                .sameSite("None")//요청부와 응답부 도메인이 같아야하는가?
+                .path("/auth/refresh")//이 경로로 사작하는 요청에만 이 쿠키를 자동으로 포함 시킴
+                .build();
     }
 }
