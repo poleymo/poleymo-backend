95fb3d4fd89a35ee60fb4e15f303f742e31ae976
K-KY <kyuyoungk@naver.com>
2026-01-16 23:52:51 +0900

    feat: 리프레시 토큰 요청 dto 제거 응답 dto에서 리프레시 토큰 필드 제거
    - 쿠키로 넣어줄거라 바디에서는 전부 제거

--- module-auth/src/main/java/com/auth/dto/AuthenticationDto.java
+++ module-auth/src/main/java/com/auth/dto/AuthenticationDto.java
@@ -4,17 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 public class AuthenticationDto {
-
-    @Getter
-    @AllArgsConstructor
-    public static class Request {
-        private String refreshToken;
-    }
-
     @Getter
     @AllArgsConstructor
     public static class Response {
-        private String refreshToken;
         private String accessToken;
     }
 }
