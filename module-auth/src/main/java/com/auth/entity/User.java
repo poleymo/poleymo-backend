4c659e78110c89fa6086eeeba204f40750c2ab70
K-KY <kyuyoungk@naver.com>
2026-01-20 03:24:52 +0900

    feat: 칼럼 제약조건 지정

--- module-auth/src/main/java/com/auth/entity/User.java
+++ module-auth/src/main/java/com/auth/entity/User.java
@@ -1,5 +1,6 @@
 package com.auth.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -23,15 +24,17 @@ public class User {
     private Date birthday;
 
     //개인 정보
+    @Column(nullable = false, unique = true)
     private String userEmail;
 
+    @Column(unique = true)
     private String userPhone;
-
 
     //학교 정보
     //사용자 정보와 학교 정보로 나눠도 될 것 같음
     private Integer grade; //학년
 
+    @Column(unique = true)
     private Long studentId; //학번
 
     private String subject; //학과
