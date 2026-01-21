94dbfcbdd42feb4766e27921c02c5586c27287ef
K-KY <kyuyoungk@naver.com>
2026-01-21 22:06:31 +0900

    feat: User 엔티티 dto 변환 메서드

--- module-auth/src/main/java/com/auth/dto/UserDto.java
+++ module-auth/src/main/java/com/auth/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.auth.dto;
 
+import com.auth.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,13 +13,14 @@ public class UserDto {
     @Builder
     @AllArgsConstructor
     public static class Request {
+
         private Long userSeq;
         private String userName;
         private String userEmail;
         private String userPhone;
         private Date birthday;
-
         private Integer grade; //학년
+
         private Long studentId; //학번
         private String subject; //학과
         private String campus;
@@ -28,15 +30,30 @@ public class UserDto {
     @Builder
     @AllArgsConstructor
     public static class Response {
+
         private Long userSeq;
         private String userName;
         private String userEmail;
         private String userPhone;
         private Date birthday;
-
         private Integer grade; //학년
+
         private Long studentId; //학번
         private String subject; //학과
         private String campus;
+    }
+
+    public static Response of(User userInfo) {
+        return Response.builder()
+                .userSeq(userInfo.getUserSeq())
+                .userEmail(userInfo.getUserEmail())
+                .userName(userInfo.getUserName())
+                .userSeq(userInfo.getUserSeq())
+                .birthday(userInfo.getBirthday())
+                .grade(userInfo.getGrade())
+                .studentId(userInfo.getStudentId())
+                .subject(userInfo.getSubject())
+                .campus(userInfo.getCampus())
+                .build();
     }
 }
