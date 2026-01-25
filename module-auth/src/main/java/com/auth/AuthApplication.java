47f4e2820d4615f8e61bb238dd5be9c677ba887b
K-KY <kyuyoungk@naver.com>
2026-01-26 08:36:53 +0900

    feat: jwt 모듈을 컴포넌트 스캔 범위에 추가

--- module-auth/src/main/java/com/auth/AuthApplication.java
+++ module-auth/src/main/java/com/auth/AuthApplication.java
@@ -2,6 +2,12 @@ package com.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {
+        "com.auth",
+        "jwt" // JwtProvider 패키지
+})
 
 @SpringBootApplication
 public class AuthApplication {
