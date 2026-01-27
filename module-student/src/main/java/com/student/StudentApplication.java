032ee4527cc899526c03865a78c8ad8136497828
K-KY <kyuyoungk@naver.com>
2026-01-28 02:29:07 +0900

    feat: 컴포넌트 스캔 범위, JPA 어노테이션 추가

--- module-student/src/main/java/com/student/StudentApplication.java
+++ module-student/src/main/java/com/student/StudentApplication.java
@@ -2,8 +2,15 @@ package com.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@ComponentScan(basePackages = {
+        "com.student",
+        "jwt"
+})
 @SpringBootApplication
+@EnableJpaAuditing
 public class StudentApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentApplication.class, args);
