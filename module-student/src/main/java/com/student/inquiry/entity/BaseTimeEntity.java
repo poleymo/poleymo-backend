89a3485c1f5049bea23193d5dd20df7da5cf48cf
K-KY <kyuyoungk@naver.com>
2026-01-28 02:29:43 +0900

    feat: 기본 엔티티

---
+++ module-student/src/main/java/com/student/inquiry/entity/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.student.inquiry.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+    @CreatedBy
+    @Column(updatable = false)
+    private Long createdBy;
+
+    @LastModifiedBy
+    private Long updatedBy;
+}
